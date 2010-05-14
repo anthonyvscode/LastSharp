4e15661748bfe2e8d4c13b894be76763f0a372e3
dkarzon <dkarzon@tboda.com>
2010-05-14 21:50:28 +1000

    Fixed the Ticket Response Class to not have a property called Ticket

--- Lastfm/Model/Event/Ticket.cs
+++ Lastfm/Model/Event/Ticket.cs
@@ -3,7 +3,7 @@ namespace Lastfm.Model
 {
     public class Ticket
     {
-        public string Ticket { get; set; }
+        public string Value { get; set; }
         public string Supplier { get; set; }
     }
 }
